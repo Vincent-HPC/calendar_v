651018dea38ff2bde6e8df040dc369b4f3e68f91
Vincent <pchuang92738@gmail.com>
2018-08-17 10:45:40 +0800

    make calendar structure

---
+++ public_html/data.php
@@ -0,0 +1,15 @@
+<?php
+
+$dates = [];
+
+for($i = 1; $i <= 31; $i++) {
+  $dates[] = $i;
+}
+
+$dates[] = null;
+$dates[] = null;
+$dates[] = null;
+$dates[] = null;
+
+
+?>
--- public_html/index.php
+++ public_html/index.php
@@ -1,14 +1,37 @@
-<?php
+<?php include('header.php') ?>
+<?php include('data.php') ?>
+
+  <div id="calendar">
+    <div id="header">
+      <?= date('Y') ?>/<?= date('m') ?>
+    </div>
+    
+    <div id="days" class="clearfix">
+      <div class="day">Sun</div>
+      <div class="day">Mon</div>
+      <div class="day">Tue</div>
+      <div class="day">Wed</div>
+      <div class="day">Thu</div>
+      <div class="day">Fri</div>
+      <div class="day">Sat</div>
+    </div>
+    
+    <div id="dates" class="clearfix">
+      <?php foreach ($dates as $key => $date) : ?>
+        <div class="date-block <?= (is_null($date))? 'empty' : ''?> ">
+          <div class="date"><?= $date ?></div>
+          <div class="events"></div>
+        </div>
 
 
+      <?php endforeach ?>
+    </div>
 
-echo("welcome");
-
-
-
-
-
-
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, obcaecati dolores. Nihil voluptatem velit consectetur hic voluptates quis iste magnam sunt aperiam eum rerum est, aut minus! Inventore, accusamus recusandae!
+  </div>
 
 
-?>
+  </div>
+
+<?php include('footer.php') ?>
